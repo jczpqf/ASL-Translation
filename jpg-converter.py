4989a82db6828f7de0217eb469843b24c83cac30
jczpqf <joshzygmunt@gmail.com>
2018-10-28 14:17:04 -0500

    added feature to resize images

--- jpg-converter.py
+++ jpg-converter.py
@@ -1,10 +1,6 @@
 """
 Converts jpg images to a single channel numpy arrays
 Run this file from the directory where the image folder is contained
-
-Takes command line arguments
-    The first argument is the folder where the images are contained
-    The second argument is the name of the new folder to store the new images
 """
 
 import os
@@ -22,12 +18,14 @@ parser.add_argument(
 parser.add_argument(
     '--sub_dir_labels', type=bool, default=True
 )
+parser.add_argument('--new_dim', type=int, default=25)
 
 args = parser.parse_args()
 
 image_dir = args.image_dir
 new_dir = args.new_dir
 sub_dir_labels = args.sub_dir_labels
+new_dim = args.new_dim
 
 
 def remove_extension(file_name):
@@ -40,13 +38,24 @@ def remove_extension(file_name):
     return file_name
 
 
+def transform(img):
+    """
+    transforms the images to greyscale downscaled image and transforms
+    it into a numpy array
+    """
+    img = img.convert('L')  # convert image to greyscale
+    img = img.resize((new_dim, new_dim), Image.ANTIALIAS)
+    img = np.array(img)  # convert image to numpy array
+    return img
+
+
 root_dir = os.getcwd()
-path_labels = os.listdir(root_dir + '\\' + image_dir)
 try:
     os.mkdir(root_dir + '\\' + new_dir)
 except FileExistsError:  # if directory already exists thats ok
     pass
 if sub_dir_labels:
+    path_labels = os.listdir(root_dir + '\\' + image_dir)
     for label in path_labels:
         try:
             os.mkdir(root_dir + '\\' + new_dir + '\\' + label)
@@ -56,8 +65,7 @@ if sub_dir_labels:
         for file in files:
             img = Image.open(root_dir + '\\' + image_dir + '\\' +
                              label + '\\' + file)  # open image
-            img = img.convert('L')  # convert image to greyscale
-            img = np.array(img)  # convert image to numpy array
+            img = transform(img)
             np.save(root_dir + '\\' + new_dir + '\\' +
                     label + '\\' + remove_extension(file), img)
 else:
@@ -66,6 +74,5 @@ else:
         # open image
         img = Image.open(root_dir + '\\' + image_dir + '\\' + file)
 
-        img = img.convert('L')  # convert image to greyscale
-        img = np.array(img)  # convert image to numpy array
+        img = transform(img)
         np.save(root_dir + '\\' + new_dir + '\\' + remove_extension(file), img)
