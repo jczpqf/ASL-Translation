0f583582e73bbe5eb18808139d76970356437efa
jczpqf <joshzygmunt@gmail.com>
2018-10-28 14:22:13 -0500

    modified keras learner to standardize data more

--- keras-learner.py
+++ keras-learner.py
@@ -6,23 +6,23 @@ from utility import load_data, one_hot
 import numpy as np
 
 X, labels = load_data('./grey_images')
-X = X.reshape(-1, 200, 200, 1)
+X = X.reshape(-1, 25, 25, 1)
+X = X.astype('float32')
+X /= 255
 unique_y = unique(labels)
 num_unique_labels = len(unique_y)
-mapping = {label: one_hot(i) for i, label in enumerate(unique_y)}
+mapping = {label: one_hot(i, num_unique_labels)
+           for i, label in enumerate(unique_y)}
 y = np.array([mapping[label] for label in labels])
+y = y.astype('float32')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 model = Sequential()
 
-model.add(Conv2D(32, 3, 3, activation='relu', input_shape=(200, 200, 1)))
-model.add(Conv2D(32, 3, 3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Flatten(input_shape=(25, 25, 1)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dense(num_unique_labels, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -30,7 +30,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(X_train, y_train,
-          batch_size=32, epochs=10, verbose=1)
+          batch_size=64, epochs=10, verbose=1)
 
 score = model.evaluate(X_test, y_test, verbose=1)
 print(score)
