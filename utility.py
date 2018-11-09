e1a3a3d10b60712ea11d651d8ceeb215fe211abc
jczpqf <joshzygmunt@gmail.com>
2018-11-09 00:10:24 -0600

    fixed a problem in train_test_split that caused all samples to be sorted

--- utility.py
+++ utility.py
@@ -49,7 +49,9 @@ def train_test_split(X, y, test_size=0.1):
     N = len(X)
     test_set_size = int(N * test_size)
     test_indices = random.sample(range(0, N), test_set_size)
+    random.shuffle(test_indices)
     X_test, y_test = X[test_indices], y[test_indices]
     train_indicies = list(set(range(0, N)) - set(test_indices))
+    random.shuffle(train_indicies)
     X_train, y_train = X[train_indicies], y[train_indicies]
     return X_train, X_test, y_train, y_test
