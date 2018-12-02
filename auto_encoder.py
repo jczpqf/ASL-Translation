70e506ffd9a36c6c33e43f963a28d4c533ef68f0
jczpqf <joshzygmunt@gmail.com>
2018-12-01 20:03:49 -0600

    added greedy train

--- auto_encoder.py
+++ auto_encoder.py
@@ -55,3 +55,34 @@ class AutoEncoder(nn.Module):
             if verbose:
                 print(epoch_num, epoch_loss)
         return self
+
+    def train_greedy(self, X, batch_size, epochs, loss_fn=None, verbose=None):
+        if loss_fn is None:
+            loss_fn = nn.MSELoss(reduction='sum')
+        N = len(X)
+        num_layers = len(self.encoder_layers)
+        for i in range(num_layers):
+            params = [param for param in
+                      self.encoder_layers[i].parameters()] + \
+                     [param for param in
+                      self.decoder_layers[num_layers - i - 1].parameters()]
+            op = torch.optim.Adam(params=params)
+            for epoch_num in range(epochs):
+                epoch_loss = 0
+                training = batch_generator(X, batch_size, shuffle=True)
+                for x_set in training:
+                    x_set = x_set[0]
+                    ouput = x_set
+                    op.zero_grad()
+                    for layer in self.encoder_layers[:i + 1]:
+                        ouput = F.softmax(layer(ouput), 1)
+                    for layer in self.decoder_layers[num_layers - i - 1:-1]:
+                        ouput = F.softmax(layer(ouput), 1)
+                    ouput = self.decoder_layers[-1](ouput)
+                    loss = loss_fn(ouput, x_set)
+                    loss /= N
+                    epoch_loss += float(loss)
+                    loss.backward()
+                    op.step()
+                if verbose:
+                    print(i, epoch_num, epoch_loss)
